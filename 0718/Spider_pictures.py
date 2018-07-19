b33ede08961a5b6721e9cd370241b3030283236e
Your Name <you@example.com>
2018-07-19 16:55:59 +0800

    nothing

--- 0718/Spider_pictures.py
+++ 0718/Spider_pictures.py
@@ -1,7 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+'''
+爬取网站的图片
+'''
 
 response = requests.get('https://tieba.baidu.com/f?ie=utf-8&kw=%E6%90%9E%E7%AC%91%E5%9B%BE%E7%89%87%E5%90%A7&fr=search')
 soup = BeautifulSoup(response.content, 'html.parser')
-img = soup.find_all('img')
-print(img)
+imgs = soup.find_all('img')
+urls = []
+
+for img in imgs:
+    src = img['src']
+    bpic = img.get('bpic')
+    if bpic:
+        urls.append(bpic)
+    if src and src.find('.jpg') != -1:
+        urls.append(src)
+i = 0
+
+for url in urls:
+    r_page = requests.get(url)
+    with open(r'pictures' + os.sep + str(i) + '.jpg', 'wb') as p:
+        p.write(r_page.content)
+    i += 1
+print('抓取完成')
+
+
---
+++ 0719/AboutExcel.py
@@ -0,0 +1,22 @@
+# ！usr/bin/env python
+
+__author__ = 'Camille'
+'''
+data from student.txt to test1.xlsx
+'''
+from openpyxl import load_workbook, Workbook
+import os
+import json
+
+# new_excel = os.getcwd() + os.sep+ "test1.xlsx"
+# print(new_excel)
+wb = Workbook()
+sheet = wb.active
+sheet.title = 'sheet1'
+with open('student.txt', encoding='utf-8') as s:
+    student = json.loads(s.read())
+print(student)
+for i in student:
+    student[i].insert(0, i)
+    sheet.append(student[i])
+wb.save('test1.xlsx')
